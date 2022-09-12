99b031bdf8f1edcc89d51f96a5cfa0af92384535
Connergifford <Conner.Gifford6@gmail.com>
2022-09-12 11:47:10 -0400

    magic.py

---
+++ week 2/functions/functions.py
@@ -0,0 +1,8 @@
+    # DRY - Don't Repeat yourself
+    # DRY - Don't Repeat yourself
+    # DRY - Don't Repeat yourself
+    # DRY - Don't Repeat yourself
+
+    def directions():
+        print("turn left out of parking lot")
+        
